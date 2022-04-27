d4ded544b67815194efa4f394cfe087791d63b95
Sean Thompson <seathomp1@WKMZTA6B7AB5.global.publicisgroupe.net>
2022-04-27 11:46:21 +1000

    clusters

--- api/index.js
+++ api/index.js
@@ -61,11 +61,18 @@ var root_exports = {};
 __export(root_exports, {
   MyMapComponent: () => MyMapComponent,
   default: () => App,
+  loader: () => loader,
   meta: () => meta
 });
 var import_react2 = require("@remix-run/react");
 var import_react_wrapper = require("@googlemaps/react-wrapper");
 var import_react3 = require("react");
+var import_markerclusterer = require("@googlemaps/markerclusterer");
+async function loader() {
+  return {
+    REACT_GOOGLE_MAPS_API_KEY: process.env.REACT_GOOGLE_MAPS_API_KEY
+  };
+}
 var meta = () => ({
   charset: "utf-8",
   title: "New Remix App",
@@ -78,19 +85,41 @@ var renderMappy = (status) => {
 };
 var MyMapComponent = ({ children }) => {
   const ref = (0, import_react3.useRef)(null);
-  const [map, setMap] = (0, import_react3.useState)();
+  const [map2, setMap] = (0, import_react3.useState)();
   (0, import_react3.useEffect)(() => {
-    if (ref.current && !map) {
+    if (ref.current && !map2) {
       var latlng = new google.maps.LatLng(-34.397, 150.644);
       setMap(new window.google.maps.Map(ref.current, { zoom: 8, center: latlng }));
     }
-  }, [ref, map]);
+  }, [ref, map2]);
+  const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const locations = [
+    { lat: -31.56391, lng: 147.154312 },
+    { lat: -33.718234, lng: 150.363181 },
+    { lat: -33.727111, lng: 150.371124 },
+    { lat: -33.848588, lng: 151.209834 },
+    { lat: -33.851702, lng: 151.216968 },
+    { lat: -34.671264, lng: 150.863657 },
+    { lat: -35.304724, lng: 148.662905 },
+    { lat: -36.817685, lng: 175.699196 },
+    { lat: -36.828611, lng: 175.790222 },
+    { lat: -37.75, lng: 145.116667 }
+  ];
+  const markers = locations.map((position, i) => {
+    const label = labels[i % labels.length];
+    const marker = new google.maps.Marker({
+      position,
+      label
+    });
+    return marker;
+  });
+  new import_markerclusterer.MarkerClusterer({ markers, map: map2 });
   return /* @__PURE__ */ React.createElement(React.Fragment, null, /* @__PURE__ */ React.createElement("div", {
     ref,
     style: { width: 800, height: 600 }
   }), import_react3.Children.map(children, (child) => {
     if ((0, import_react3.isValidElement)(child)) {
-      return (0, import_react3.cloneElement)(child, { map });
+      return (0, import_react3.cloneElement)(child, { map: map2 });
     }
   }));
 };
@@ -117,10 +146,11 @@ var Marker = (options) => {
   return null;
 };
 function App() {
+  const envData = (0, import_react2.useLoaderData)();
   return /* @__PURE__ */ React.createElement("html", {
     lang: "en"
   }, /* @__PURE__ */ React.createElement("head", null, /* @__PURE__ */ React.createElement(import_react2.Meta, null), /* @__PURE__ */ React.createElement(import_react2.Links, null)), /* @__PURE__ */ React.createElement("body", null, /* @__PURE__ */ React.createElement(import_react_wrapper.Wrapper, {
-    apiKey: "AIzaSyAnOd-ezmIIpuGq1QUyPkMBD0Omsx6qy60",
+    apiKey: envData.REACT_GOOGLE_MAPS_API_KEY,
     render: renderMappy
   }, /* @__PURE__ */ React.createElement(MyMapComponent, null, /* @__PURE__ */ React.createElement(Marker, null))), /* @__PURE__ */ React.createElement(import_react2.Outlet, null), /* @__PURE__ */ React.createElement(import_react2.ScrollRestoration, null), /* @__PURE__ */ React.createElement(import_react2.Scripts, null), /* @__PURE__ */ React.createElement(import_react2.LiveReload, null)));
 }
@@ -149,7 +179,7 @@ function Index() {
 }
 
 // server-assets-manifest:@remix-run/dev/assets-manifest
-var assets_manifest_default = { "version": "c9296ab3", "entry": { "module": "/build/entry.client-66GSKIVS.js", "imports": ["/build/_shared/chunk-KZIW5VEW.js", "/build/_shared/chunk-XV23MX66.js"] }, "routes": { "root": { "id": "root", "parentId": void 0, "path": "", "index": void 0, "caseSensitive": void 0, "module": "/build/root-JZX2ITGO.js", "imports": void 0, "hasAction": false, "hasLoader": false, "hasCatchBoundary": false, "hasErrorBoundary": false }, "routes/index": { "id": "routes/index", "parentId": "root", "path": void 0, "index": true, "caseSensitive": void 0, "module": "/build/routes/index-SJ5NMTVZ.js", "imports": void 0, "hasAction": false, "hasLoader": false, "hasCatchBoundary": false, "hasErrorBoundary": false } }, "url": "/build/manifest-C9296AB3.js" };
+var assets_manifest_default = { "version": "3c1f4f47", "entry": { "module": "/build/entry.client-XWLXNKVG.js", "imports": ["/build/_shared/chunk-PBUWZJJC.js", "/build/_shared/chunk-XV23MX66.js"] }, "routes": { "root": { "id": "root", "parentId": void 0, "path": "", "index": void 0, "caseSensitive": void 0, "module": "/build/root-NHFJZR6G.js", "imports": void 0, "hasAction": false, "hasLoader": true, "hasCatchBoundary": false, "hasErrorBoundary": false }, "routes/index": { "id": "routes/index", "parentId": "root", "path": void 0, "index": true, "caseSensitive": void 0, "module": "/build/routes/index-SJ5NMTVZ.js", "imports": void 0, "hasAction": false, "hasLoader": false, "hasCatchBoundary": false, "hasErrorBoundary": false } }, "url": "/build/manifest-3C1F4F47.js" };
 
 // server-entry-module:@remix-run/dev/server-build
 var entry = { module: entry_server_exports };
--- app/root.tsx
+++ app/root.tsx
@@ -6,10 +6,18 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
 
 import { useEffect, useRef, useState, ReactElement, Children, isValidElement, cloneElement } from "react";
+import { MarkerClusterer } from "@googlemaps/markerclusterer";
+
+export async function loader() {
+  return {
+    REACT_GOOGLE_MAPS_API_KEY: process.env.REACT_GOOGLE_MAPS_API_KEY,
+  };
+}
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
@@ -33,6 +41,40 @@ export const MyMapComponent = ({ children }: { children: any }) => {
     }
   }, [ref, map]);
 
+
+  const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const locations = [
+    { lat: -31.56391, lng: 147.154312 },
+    { lat: -33.718234, lng: 150.363181 },
+    { lat: -33.727111, lng: 150.371124 },
+    { lat: -33.848588, lng: 151.209834 },
+    { lat: -33.851702, lng: 151.216968 },
+    { lat: -34.671264, lng: 150.863657 },
+    { lat: -35.304724, lng: 148.662905 },
+    { lat: -36.817685, lng: 175.699196 },
+    { lat: -36.828611, lng: 175.790222 },
+    { lat: -37.75, lng: 145.116667 }];
+
+  const markers = locations.map((position, i) => {
+    const label = labels[i % labels.length];
+    const marker = new google.maps.Marker({
+      position,
+      label,
+    });
+
+    // markers can only be keyboard focusable when they have click listeners
+    // open info window when marker is clicked
+    /*
+    marker.addListener("click", () => {
+      infoWindow.setContent(label);
+      infoWindow.open(map, marker);
+    });
+    */
+
+    return marker;
+  });
+  new MarkerClusterer({ markers, map });
+
   return (
     <>
       <div ref={ref} style={{ width: 800, height: 600 }} />
@@ -44,8 +86,6 @@ export const MyMapComponent = ({ children }: { children: any }) => {
       })}
     </>
   );
-
-  // return <div ref={ref} id="map" style={{ width: 800, height: 600, background: 'yellow', overflow: 'visible' }} />
 }
 
 const Marker: React.FC<google.maps.MarkerOptions> = (options) => {
@@ -76,7 +116,50 @@ const Marker: React.FC<google.maps.MarkerOptions> = (options) => {
   return null;
 };
 
+const Clusters: React.FC<google.maps.MarkerOptions> = (options) => {
+  const [marker, setMarker] = useState<google.maps.Marker>();
+
+  const locations = [
+    { lat: -31.56391, lng: 147.154312 },
+    { lat: -33.718234, lng: 150.363181 },
+    { lat: -33.727111, lng: 150.371124 },
+    { lat: -33.848588, lng: 151.209834 },
+    { lat: -33.851702, lng: 151.216968 },
+    { lat: -34.671264, lng: 150.863657 },
+    { lat: -35.304724, lng: 148.662905 },
+    { lat: -36.817685, lng: 175.699196 },
+    { lat: -36.828611, lng: 175.790222 },
+    { lat: -37.75, lng: 145.116667 }];
+
+  const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  // Add some markers to the map.
+  const markers = locations.map((position, i) => {
+    const label = labels[i % labels.length];
+    const marker = new google.maps.Marker({
+      position,
+      label,
+    });
+
+    // markers can only be keyboard focusable when they have click listeners
+    // open info window when marker is clicked
+    /*
+    marker.addListener("click", () => {
+      infoWindow.setContent(label);
+      infoWindow.open(map, marker);
+    });
+    */
+
+    return marker;
+  });
+
+  // Add a marker clusterer to manage the markers.
+  new MarkerClusterer({ markers, map });
+};
+
 export default function App() {
+  const envData = useLoaderData();
+
   return (
     <html lang="en">
       <head>
@@ -84,7 +167,7 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Wrapper apiKey={"AIzaSyAnOd-ezmIIpuGq1QUyPkMBD0Omsx6qy60"} render={renderMappy}>
+        <Wrapper apiKey={envData.REACT_GOOGLE_MAPS_API_KEY} render={renderMappy}>
           <MyMapComponent>
             <Marker />
           </MyMapComponent>
